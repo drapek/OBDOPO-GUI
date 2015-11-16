c4c9d73019c8ddf7966594d2f7ee8ca4d0638f80
drapek <drapek39@wp.pl>
2015-11-16 21:19:50 +0100

    Naprawiony błąd numerowania punktów w tabeli po operacji usuwania.

--- src/MainWindowLogic.java
+++ src/MainWindowLogic.java
@@ -1,6 +1,7 @@
 
 import java.io.File;
 import ProjectExceptions.*;
+import java.awt.geom.Point2D;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -40,14 +41,14 @@ public class MainWindowLogic {
         // percentage of text signs in the text
         if (!(d > 0.950))
             throw new IncorrectFileTypeException();
-
+        
         }
     
     static void addRowToTable(JTable where) {
         DefaultTableModel tmp = (DefaultTableModel) where.getModel();
         int acutalsize = tmp.getRowCount();
         acutalsize++;
-        tmp.addRow(new Object[]{acutalsize + ".", 0, 0, false});
+        tmp.addRow(new Object[]{acutalsize + ".", 0.0, 0.0, false});
     }
     
     static void deleteSelectedRow(JTable where) {
@@ -72,6 +73,59 @@ public class MainWindowLogic {
             tmp.setValueAt(val, i, 0);
             
             
+        }
+    }
+    
+    static void readPointsFile(File fil_hndl, JTable tabela) throws IOException, InvalidFormatInFileException {
+        ReadPointsFromFile pointsReader = new ReadPointsFromFile(fil_hndl.getPath());
+        System.out.println("Ścieżka wcztytywanego pliku: " + fil_hndl.getPath());
+        Point2D [] readed = pointsReader.readPoints();
+        
+        DefaultTableModel defaultModelTabeli = (DefaultTableModel) tabela.getModel(); 
+        
+        for (Point2D point : readed ) {
+            defaultModelTabeli.addRow(new Object [] { defaultModelTabeli.getRowCount() + 1 + ".", point.getX(), point.getY(), false });
+        }
+       
+       
+    }
+    
+    static Point2D[] rewritePointsTableWitoutDuplicats(JTable jTable1) {
+        deleteDuplicatsFromTable(jTable1);
+        DefaultTableModel tmp = (DefaultTableModel) jTable1.getModel();
+        
+        Point2D[] rewritedTableFromTable = new Point2D[tmp.getRowCount()];
+        for( int i = 0; i < rewritedTableFromTable.length; i++) {
+            rewritedTableFromTable[i] = new Point2D.Double((double) tmp.getValueAt(i, 1), (double) tmp.getValueAt(i, 2));
+        }
+       
+        return rewritedTableFromTable;
+    }
+    
+    private static void deleteDuplicatsFromTable(JTable jTable) {
+        DefaultTableModel tmp = (DefaultTableModel) jTable.getModel();
+        boolean somethingChange = false;
+        for( int i = 0; i < tmp.getRowCount(); i++) {
+            for(int j = i + 1; j < tmp.getRowCount(); j++) {
+         
+                if(( (double) tmp.getValueAt(i, 1) == (double) tmp.getValueAt(j, 1)) &&
+                   ( (double) tmp.getValueAt(i, 2) == (double) tmp.getValueAt(j, 2))) {
+                    tmp.removeRow(j);
+                    j--;
+                    somethingChange = true;
+                    
+                }
+            }
+        }
+        if( somethingChange ) {
+            ponumerujWiersze(jTable);
+        }
+    }
+    
+    static void inputValuesIntoTableFromDataForwarder(Point2D [] pointsCollection, JTable jTabela) {
+        DefaultTableModel tmp = (DefaultTableModel) jTabela.getModel();
+        for( Point2D point : pointsCollection) {
+            tmp.addRow(new Object[]{(tmp.getRowCount() + 1) + ".", point.getX(), point.getY(), false});
         }
     }
 }
